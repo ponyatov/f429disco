102d7886bdb134b9e8aaa024a20e9b8c42e5268a
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-08 15:44:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- build.rs
+++ build.rs
@@ -2,30 +2,20 @@
 use std::fs;
 use std::path::Path;
 
-#[cfg!(feature = "f429disco")]
-const memoryx = "hw/f429disco/f429disco.x";
+#[cfg(feature = "f429disco")]
+const MEMORY_X: &str = "hw/f429disco/f429disco.x";
+#[cfg(feature = "l496disco")]
+const MEMORY_X: &str = "hw/l496disco/l496disco.x";
+#[cfg(feature = "netduinoplus2")]
+const MEMORY_X: &str = "hw/netduinoplus2/netduinoplus2.x";
+#[cfg(feature = "iskra")]
+const MEMORY_X: &str = "hw/iskra/iskra.x";
 
 fn main() {
-    let (board, memoryx) = //
-    if cfg!(feature = "f429disco") {
-        ("f429disco", "hw/f429disco/f429disco.x")
-    } else if cfg!(feature = "netduinoplus2") {
-        ("netduinoplus2", "hw/netduinoplus2/netduinoplus2.x")
-    } else if cfg!(feature = "l496disco") {
-        ("l496disco", "hw/l496disco/l496disco.x")
-    } else if cfg!(feature = "pillf103") {
-        ("pillf103", "hw/pillf103/pillf103.x")
+    if Path::new(MEMORY_X).exists() {
+        fs::copy(MEMORY_X, "memory.x").unwrap();
+        println!("cargo:rerun-if-changed={}", MEMORY_X);
     } else {
-        ("iskra", "hw/iskra/iskra.x")
-    };
-
-    if Path::new(memoryx).exists() {
-        fs::copy(memoryx, "memory.x").unwrap();
-        println!("cargo:rerun-if-changed={}", memoryx);
-    } else {
-        panic!(
-            "Memory layout file not found for board {}: {}",
-            board, memoryx
-        );
+        let _ = fs::remove_file("memory.x");
     }
 }
--- hw/pillf103/pillf103.x
+++
@@ -1,5 +0,0 @@
-MEMORY
-{
-  FLASH (r x) : ORIGIN = 0x08000000, LENGTH = 64K
-  RAM   (rwx) : ORIGIN = 0x20000000, LENGTH = 20K
-}
