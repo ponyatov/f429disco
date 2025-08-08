3097712d8683b08c0cf114da42bcc7d7520b139a
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-08 16:50:49 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/readline.rs
+++ src/readline.rs
@@ -1,39 +1,46 @@
 // libreadline analog for pure no_std CortexM & embedded Linux
 
-use heapless::String;
-
 pub struct ReadLine<const N: usize> {
-    buffer: String<N>,
+    buffer: [u8; N],
+    len: usize,
     cursor: usize,
 }
 
 impl<const N: usize> ReadLine<N> {
     pub fn new() -> Self {
         Self {
-            buffer: String::new(),
+            buffer: [0; N],
+            len: 0,
             cursor: 0,
         }
     }
 
-    pub fn input(&mut self, ch: char) -> Option<String<N>> {
+    pub fn input(&mut self, ch: char) -> Option<&str> {
         match ch {
             '\r' | '\n' => {
-                let line = self.buffer.clone();
-                self.buffer.clear();
+                let result = core::str::from_utf8(&self.buffer[..self.len]).ok();
+                self.len = 0;
                 self.cursor = 0;
-                Some(line)
+                result
             }
             '\x08' | '\x7f' => { // Backspace/DEL
                 if self.cursor > 0 {
-                    self.buffer.remove(self.cursor - 1);
                     self.cursor -= 1;
+                    self.len -= 1;
+                    for i in self.cursor..self.len {
+                        self.buffer[i] = self.buffer[i + 1];
+                    }
                 }
                 None
             }
             c if c.is_ascii_graphic() || c == ' ' => {
-                if self.buffer.len() < N {
-                    let _ = self.buffer.insert(self.cursor, c);
+                if self.len < N {
+                    for i in (self.cursor..self.len).rev() {
+                        self.buffer[i + 1] = self.buffer[i];
+                    }
+                    self.buffer[self.cursor] = c as u8;
                     self.cursor += 1;
+                    self.len += 1;
                 }
                 None
             }
@@ -42,7 +49,7 @@ impl<const N: usize> ReadLine<N> {
     }
 
     pub fn buffer(&self) -> &str {
-        &self.buffer
+        core::str::from_utf8(&self.buffer[..self.len]).unwrap_or("")
     }
 
     pub fn cursor(&self) -> usize {
